1921fa2089432d8ce1747f7ff7689e4f694e29fe
DESKTOP-J7R10IJ\MINH <qmin123443@gmail.com>
2021-03-03 22:02:45 +0700

    ad

--- Lab2/src/Bai3/UnetiBook.java
+++ Lab2/src/Bai3/UnetiBook.java
@@ -85,9 +85,94 @@ public class UnetiBook extends Book {
             System.out.println("khong co sach ban muon tim");
         }
     }
-    
-    public void searchNameAuthor(UnetiBook[] b){
-        
+
+    public void searchNameAuthor(UnetiBook[] b) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten tac gia:");
+        String nameAuthor = sc.nextLine();
+        int dem = 0;
+        System.out.println("thong tin tac gia ban muon tim la :");
+        for (int i = 0; i < b.length; i++) {
+            if (b[i].getBookAuthor().equals(nameAuthor)) {
+                b[i].display();
+                dem++;
+            }
+        }
+        if (dem == 0) {
+            System.out.println("khong co tac gia ban muon tim");
+        }
     }
-    
+
+    void menu() {
+        System.out.println("1 nhap thong tin n cuon sach ");
+        System.out.println("2 hien thi thong tin vua nhap");
+        System.out.println("3 sap xep giam dan theo nam xuat ban");
+        System.out.println("4 tim kiem theo ten sach");
+        System.out.println("5 tim kiem theo ten tac gia ");
+        System.out.println("6 thoat");
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        UnetiBook ab = new UnetiBook();
+        UnetiBook[] ab1 = null;
+        do {
+            ab.menu();
+            System.out.println("Nhap vao lua chon cua ban :");
+            Scanner sc = new Scanner(System.in);
+            n = Integer.parseInt(sc.nextLine());
+            switch (n) {
+                case 1: {
+                    int m;
+                    System.out.println("Nhap vao n :");
+                    m = Integer.parseInt(sc.nextLine());
+                    ab1 = new UnetiBook[m];
+                    for (int i = 0; i < m; i++) {
+                        ab1[i] = new UnetiBook();
+                        ab1[i].input();
+                    }
+                    break;
+                }
+                case 2: {
+                    if (ab1 == null) {
+                        System.out.println("ban chua nhap du lieu");
+                    } else {
+                        System.out.println("du lieu ban vua nhap la :");
+                        for (int i = 0; i < ab1.length; i++) {
+                            System.out.println("thong tin sach thu " + (i + 1));
+                            ab1[i].display();
+                        }
+                    }
+                    break;
+                }
+                case 3: {
+                    if (ab1 == null) {
+                        System.out.println("ban chua nhap du lieu");
+                    } else {
+                        ab.sort(ab1);
+                        System.out.println("du lieu sau khi sap xep la :");
+                        for (int i = 0; i < ab1.length; i++) {
+                            System.out.println("thong tin hang hoa thu " + (i + 1));
+                            ab1[i].display();
+                        }
+                    }
+                    break;
+                }
+                case 4: {
+                    ab.searchNameBook(ab1);
+                    break;
+                }
+                case 5: {
+                    ab.searchNameBook(ab1);
+                    break;
+                }
+                case 6:
+                    break;
+                default: {
+                    System.out.println("khong co lua chon cua ban ");
+                    break;
+                }
+            }
+        } while (n != 6);
+    }
 }
