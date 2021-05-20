c4e43a985af4dc755665d04a72dd3dc86df8f8a0
DESKTOP-J7R10IJ\MINH <qmin123443@gmail.com>
2021-05-20 15:57:02 +0700

    minh cm

--- Lab9ky2nam3/Lab9ky2nam3/src/bai2/NewClass.java
+++ Lab9ky2nam3/Lab9ky2nam3/src/bai2/NewClass.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Core;
+package bai2;
 
 /**
  *
- * @author MINH
+ * @author ADMIN
  */
-public class UDPServer_Thread {
+public class NewClass {
     
 }
